70a1ecc5fdb5689f7c4b775aada299ef2e1d5c7f
Dayane  Silva <72285834+Dayaslva@users.noreply.github.com>
2025-03-11 19:13:03 -0300

    Update desafio1.rb

--- Desafios/desafio1.rb
+++ Desafios/desafio1.rb
@@ -1,4 +1,5 @@
-# Neste desafio de projeto, você precisa criar um programa simples de acesso a leitores em uma biblioteca e para isso o usuário deverá digitar seu nome e sobrenome, além da sua idade que é um dado relevante para o departamento de marketing daquele local. Tudo isso, impresso em uma única frase.
+# Neste desafio de projeto, você precisa criar um programa simples de acesso a leitores em uma biblioteca e para isso o usuário deverá digitar seu nome e sobrenome, 
+#além da sua idade que é um dado relevante para o departamento de marketing daquele local. Tudo isso, impresso em uma única frase.
 
 #1)digitar seu nome 
 #2)digitar seu sobrenome
